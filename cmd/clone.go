57580b19ba26c880417e4017957f8be5c39182cb
Siddharth Singh <siddharthsingh2014@gmail.com>
2023-06-29 13:19:50 +0530

    added the clone command and tested it

--- cmd/clone.go
+++ cmd/clone.go
@@ -1,40 +1,76 @@
 /*
 Copyright © 2023 NAME HERE <EMAIL ADDRESS>
-
 */
 package cmd
 
 import (
 	"fmt"
+	"os"
+	"os/exec"
+	"strings"
 
 	"github.com/spf13/cobra"
 )
 
+var gitUrl string
+
 // cloneCmd represents the clone command
 var cloneCmd = &cobra.Command{
 	Use:   "clone",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
+	Short: "To Clone A Repository And Start NeoVim In It",
+	Long: `Use this command to clone a Repository, CD into that Repository
+and then open that Repository in Neovim.
 
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+**Note: 
+  Please Only Enter The SSH Link Of The Repository.`,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("clone called")
+		clone()
 	},
+}
+
+func clone() {
+	// Getting the repository name
+	parts := strings.Split(gitUrl, "/")
+	repoName := strings.TrimSuffix(parts[1], ".git")
+
+	// Execute commands sequentially
+
+	// Clone Command
+	cmd := exec.Command("git", "clone", gitUrl)
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+
+	err := cmd.Run()
+	if err != nil {
+		fmt.Printf("Error executing clone command: %v\n", err)
+		return
+	}
+
+	// CD Command
+	err = os.Chdir(repoName)
+	if err != nil {
+		fmt.Printf("Error executing cd command: %v\n", err)
+		return
+	}
+
+	// Nvim Command
+	cmd = exec.Command("nvim")
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+
+	err = cmd.Run()
+	if err != nil {
+		fmt.Printf("Error executing nvim command: %v\n", err)
+		return
+	}
 }
 
 func init() {
 	rootCmd.AddCommand(cloneCmd)
 
-	// Here you will define your flags and configuration settings.
-
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// cloneCmd.PersistentFlags().String("foo", "", "A help for foo")
-
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// cloneCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	cloneCmd.Flags().StringVarP(&gitUrl, "url", "u", "", "url of the repository")
+	err := cloneCmd.MarkFlagRequired("url")
+	if err != nil {
+		fmt.Println(err)
+	}
 }
